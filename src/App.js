4b534939ee3e34030a2882267281e41a26aa5b38
Sandesh <sandeshpadolkar@terralogic.com>
2020-12-22 13:33:35 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {BrowserRouter as Router, HashRouter, Switch, Route} from "react-router-dom";
 import Page1 from './Component/Page1';
--- src/Component/menubar.js
+++ src/Component/menubar.js
@@ -6,7 +6,7 @@ export class menubar extends Component {
             <div>
                 <h1 className="menuhead">Menubar</h1>
                 <div className="menu">
-                <img src="/library.jpg" /><br/><br/>
+                <img src="/library.jpg" alt="Loading"/><br/><br/>
                 <a href="#Page1">Click here to enter Page1</a><br/><br/>
                 <a href="#Page2">Click here to enter Page2</a>
                 </div>
